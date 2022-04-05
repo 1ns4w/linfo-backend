fedb010b37b5bc91b3a59502f21a585da7ab1145
ins4w <amejia.foo@gmail.com>
2022-04-05 11:00:46 -0500

    feat: add puppeteer navigation support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import puppeteer from "puppeteer"
 import Fastify from "fastify"
 import fastifyCors from "fastify-cors"
+import {} from "dotenv/config"
 
 let fast = Fastify({logger: true})
 
@@ -17,8 +18,23 @@ fast.get("/", async(request, response) => {
     response.type("text/html").send(pageDOM)
 })
 
-fast.get("/scrap/:keyword", async(request, response) => {
-    return request.params
+fast.get('/scrap/:keyword', async(request, response) => {
+    const typeOptions = {delay: 100}
+    const pageOptions = {timeout: 60000, waitUntil: 'networkidle2'}
+    const { keyword } = request.params
+    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']})
+    const page = await browser.newPage()
+    await page.goto('https://www.linkedin.com/', pageOptions)
+    await page.waitForXPath('//input[@autocomplete="username"]')
+    const usernameInput = (await page.$x('//input[@autocomplete="username"]'))[0]
+    const passwordInput = (await page.$x('//input[@autocomplete="current-password"]'))[0]
+    await usernameInput.type(process.env.LINKEDIN_BOT_EMAIL, typeOptions)
+    await passwordInput.type(process.env.LINKEDIN_BOT_PASSWORD, typeOptions)
+    await passwordInput.press('Enter')
+    await page.waitForNavigation(pageOptions)
+    await page.waitForSelector('ul')
+    const text = await (await page.goto(`https://www.linkedin.com/search/results/all/?keywords=${keyword}`, pageOptions)).text()
+    response.type("text/html").send(text)
 })
 
 const start = async () => {
