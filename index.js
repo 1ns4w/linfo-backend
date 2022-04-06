21f0c3650fd805b31fad5142c95c07a853b37ab9
Piero <matematicadivina1@gmail.com>
2022-04-05 20:07:29 -0700

    service worker

--- index.js
+++ index.js
@@ -2,40 +2,39 @@ import puppeteer from "puppeteer"
 import Fastify from "fastify"
 import fastifyCors from "fastify-cors"
 import {} from "dotenv/config"
+import { Queue, Worker } from "bullmq"
+
+let queue = new Queue('foo')
 
 let fast = Fastify({logger: true})
 
+let state = {}
+
+let scrapData
+
 fast.register(fastifyCors, {
     origin: "*",
-    methods: ["POST"]
+    methods: ["POST", "GET"]
 })
 
 fast.get("/", async(request, response) => {
-    let browser = await puppeteer.launch({headless: false})
+    let browser = await puppeteer.launch({headless: true})
     let page = await browser.newPage()
     let pageDOM = await (await page.goto('https://example.com')).text()
     await browser.close()
     response.type("text/html").send(pageDOM)
 })
 
-fast.get('/scrap/:keyword', async(request, response) => {
-    const typeOptions = {delay: 100}
-    const pageOptions = { waitUntil: 'networkidle2'}
-    const browser = await puppeteer.launch({headless: false})
-    const page = await browser.newPage()
-    await page.goto('https://www.linkedin.com/', pageOptions)
-    await page.waitForXPath('//input[@autocomplete="username"]')
-    const [usernameInput] = await page.$x('//input[@autocomplete="username"]')
-    const [passwordInput] = await page.$x('//input[@autocomplete="current-password"]')
-    await usernameInput.type(process.env.LINKEDIN_BOT_EMAIL, typeOptions)
-    await passwordInput.type(process.env.LINKEDIN_BOT_PASSWORD, typeOptions)
-    await passwordInput.press('Enter')
-    console.log("feed")
-    await page.waitForNavigation(pageOptions)
-   console.log("xd")
-    const title = page.title()
-    await browser.close()
-    return title
+fast.get('/redisqueue', async(req, res)=>{
+    const job = await queue.add('job1',{foo: 'bar' })
+    state[`${job.id}`]= false
+    return {jobid: job.id}
+})
+
+fast.get('/scrap/:id', async(request, response) => {
+    const {id} = request.params
+    if (state[id]) {return {...scrapData, status:true}}
+    return {status:false}
 })
 
 const start = async () => {
@@ -47,5 +46,30 @@ const start = async () => {
         process.exit(1)
     }
 }
+
+
+const worker = new Worker('foo', async job =>{
+    
+    const typeOptions = {delay: 100}
+    const pageOptions = { waitUntil: 'networkidle2'}
+    const browser = await puppeteer.launch({headless: true})
+    const page = await browser.newPage()
+    await page.goto('https://www.linkedin.com/', pageOptions)
+    await page.waitForXPath('//input[@autocomplete="username"]')
+    const [usernameInput] = await page.$x('//input[@autocomplete="username"]')
+    const [passwordInput] = await page.$x('//input[@autocomplete="current-password"]')
+    await usernameInput.type(process.env.LINKEDIN_BOT_EMAIL, typeOptions)
+    await passwordInput.type(process.env.LINKEDIN_BOT_PASSWORD, typeOptions)
+    await passwordInput.press('Enter')
+    await page.waitForNavigation(pageOptions)
+    const title = await page.title()
+    await browser.close()
+    scrapData = {title}
+
+})
+
+worker.on('completed', job => {
+    state[`${job.id}`]= true
+  });
 
 start()
