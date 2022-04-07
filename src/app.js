a021e17c246ab13a5189806b60d8ecdf9a4f44a0
ins4w <amejia.foo@gmail.com>
2022-04-06 21:00:20 -0500

    fixed: all timeout issues

--- src/app.js
+++ src/app.js
@@ -3,20 +3,17 @@ import puppeteer from 'puppeteer'
 
 const scrap = async (url) => {
     const typeOptions = { delay: 100 }
-    const pageOptions = { timeout: 90000, waitUntil: 'networkidle2' }
+    const pageOptions = { waitUntil: 'networkidle2' }
     const browser = await puppeteer.launch({ headless: true })
     const page = await browser.newPage()
-    await page.goto('https://www.linkedin.com/', pageOptions)
-    await page.waitForXPath('//input[@autocomplete="username"]')
+    await page.goto('https://www.linkedin.com/', { waitUntil: 'networkidle0' })
     const [usernameInput] = await page.$x('//input[@autocomplete="username"]')
     const [passwordInput] = await page.$x('//input[@autocomplete="current-password"]')
-    console.log("a")
     await usernameInput.type(process.env.LINKEDIN_BOT_EMAIL, typeOptions)
     await passwordInput.type(process.env.LINKEDIN_BOT_PASSWORD, typeOptions)
-    console.log("a")
     await passwordInput.press('Enter')
-    await page.waitForNavigation({ timeout: 90000, waitUntil: 'domcontentloaded' })
-    await page.goto(url, pageOptions)
+    await page.waitForNavigation({ waitUntil: 'domcontentloaded' })
+    await page.goto(url, { timeout: 60000, ...pageOptions})
     const people = await page.$x('//a[contains(@class, "app-aware-link") and ./span]')
     const tmpPage = await browser.newPage()
 
@@ -24,10 +21,10 @@ const scrap = async (url) => {
 
     for (const person of people) {
         const personURL = await page.evaluate(a => a.href, person)
-        await tmpPage.goto(personURL, { timeout: 200000, waitUntil: 'networkidle2' })
+        await tmpPage.goto(personURL, {timeout: 180000, ...pageOptions})
         const title = await tmpPage.title()
-        console.log(title)
         scrapedProfiles.push(title)
+        console.log(scrapedProfiles)
     }
 
     await browser.close()
